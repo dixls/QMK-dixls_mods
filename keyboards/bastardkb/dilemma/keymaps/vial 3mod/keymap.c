9d45b3ed6db657b9c622e844a955fd0a8905f2df
pixls <tha.pixls@gmail.com>
2022-08-07 22:07:19 -0400

    finish modifying 3mod keymap, more similar to base miryoku

--- keyboards/bastardkb/dilemma/keymaps/vial 3mod/keymap.c
+++ keyboards/bastardkb/dilemma/keymaps/vial 3mod/keymap.c
@@ -61,11 +61,11 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
-#define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
-       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-                               TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM
+#define LAYOUT_LAYER_BASE                                                                                    \
+       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
+       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
+       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
+                               ESC_MED, SPC_NAV, TAB_FUN, BSP_NUM, ENT_SYM, DEL_FUN
 
 /** Convenience row shorthands. */
 #define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -91,17 +91,17 @@ static uint16_t auto_pointer_layer_timer = 0;
  * from the base layer to enable auto-repeat.
  */
 #define LAYOUT_LAYER_FUNCTION                                                                 \
-    _______________DEAD_HALF_ROW_______________, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
-    ______________HOME_ROW_GACS_L______________, KC_SLCK,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
-    _______________DEAD_HALF_ROW_______________, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
-                               _______, XXXXXXX, XXXXXXX, XXXXXXX
+    _______________DEAD_HALF_ROW_______________,          KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
+    ______________HOME_ROW_GACS_L______________,          KC_SLCK,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
+    _______________DEAD_HALF_ROW_______________,          KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
+                               XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
 
 /** \brief Mouse emulation and pointer functions. */
-#define LAYOUT_LAYER_POINTER                                                                  \
-    XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, \
-    ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
-    _______, DRGSCRL, SNIPING, EEP_RST, QK_BOOT, QK_BOOT, EEP_RST, SNIPING, DRGSCRL, _______, \
-                               KC_BTN2, KC_BTN1, KC_BTN1, KC_BTN2
+#define LAYOUT_LAYER_POINTER                                                                                    \
+    XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,                   S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, \
+    ______________HOME_ROW_GACS_L______________,                   ______________HOME_ROW_GACS_R______________, \
+    _______, DRGSCRL, SNIPING, EEP_RST, QK_BOOT,                   QK_BOOT, EEP_RST, SNIPING, DRGSCRL, _______, \
+                               KC_BTN3, KC_BTN1, KC_BTN2, KC_BTN2, KC_BTN1, KC_BTN3
 
 /**
  * \brief Navigation layer.
@@ -111,11 +111,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * caps lock and insert on the inner column. Thumb keys are duplicated from the
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
-#define LAYOUT_LAYER_NAVIGATION                                                               \
-    _______________DEAD_HALF_ROW_______________, _______________DEAD_HALF_ROW_______________, \
-    ______________HOME_ROW_GACS_L______________, KC_CLCK, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, \
-    _______________DEAD_HALF_ROW_______________,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, \
-                               XXXXXXX, _______,  KC_ENT, KC_BSPC
+#define LAYOUT_LAYER_NAVIGATION                                                                                  \
+    _______________DEAD_HALF_ROW_______________,                     KC_INS, KC_HOME,   KC_UP,  KC_END, KC_PGUP, \
+    ______________HOME_ROW_GACS_L______________,                    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, \
+    _______________DEAD_HALF_ROW_______________,                      U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND, \
+                               XXXXXXX, _______, XXXXXXX,  KC_BSPC,  KC_ENT, KC_DEL
 
 /**
  * \brief Numeral layout.
@@ -124,11 +124,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * are in the standard numpad locations with symbols in the remaining positions.
  * `KC_DOT` is duplicated from the base layer.
  */
-#define LAYOUT_LAYER_NUMERAL                                                                  \
-    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______________DEAD_HALF_ROW_______________, \
-    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, ______________HOME_ROW_GACS_R______________, \
-     KC_DOT,    KC_1,    KC_2,    KC_3, KC_BSLS, _______________DEAD_HALF_ROW_______________, \
-                               KC_MINS,    KC_0, XXXXXXX, _______
+#define LAYOUT_LAYER_NUMERAL                                                                                    \
+    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                   _______________DEAD_HALF_ROW_______________, \
+    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL,                   ______________HOME_ROW_GACS_R______________, \
+     KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,                   _______________DEAD_HALF_ROW_______________, \
+                                KC_DOT,    KC_0, KC_MINS, _______, XXXXXXX, XXXXXXX,
 
 /**
  * \brief Symbols layer.
@@ -137,11 +137,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
  * `KC_RPRN`.
  */
-#define LAYOUT_LAYER_SYMBOLS                                                                  \
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______________DEAD_HALF_ROW_______________, \
-    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GACS_R______________, \
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, _______________DEAD_HALF_ROW_______________, \
-                                KC_GRV, KC_UNDS, _______, XXXXXXX
+#define LAYOUT_LAYER_SYMBOLS                                                                                    \
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                   _______________DEAD_HALF_ROW_______________, \
+    KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                   ______________HOME_ROW_GACS_R______________, \
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                   _______________DEAD_HALF_ROW_______________, \
+                               KC_LPRN, KC_RPRN, KC_UNDS, XXXXXXX, _______, XXXXXXX
 
 /**
  * \brief Add Home Row mod to a layout.
