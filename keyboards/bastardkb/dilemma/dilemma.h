1f9ed27c5f57fc63f392104c316f26b6c7408b0d
pixls <tha.pixls@gmail.com>
2022-08-07 21:26:34 -0400

    add vial 3mod keymap, change dilemma matrix to show 3 thumb keys

--- keyboards/bastardkb/dilemma/dilemma.h
+++ keyboards/bastardkb/dilemma/dilemma.h
@@ -20,21 +20,21 @@
 #include "quantum.h"
 
 // clang-format off
-#define LAYOUT_split_3x5_2(                             \
-  k00, k01, k02, k03, k04,     k44, k43, k42, k41, k40, \
-  k10, k11, k12, k13, k14,     k54, k53, k52, k51, k50, \
-  k20, k21, k22, k23, k24,     k64, k63, k62, k61, k60, \
-                 k30, k32,     k72, k70                 \
+#define LAYOUT_split_3x5_3(                             \
+  k00, k01, k02, k03, k04,                k44, k43, k42, k41, k40, \
+  k10, k11, k12, k13, k14,                k54, k53, k52, k51, k50, \
+  k20, k21, k22, k23, k24,                k64, k63, k62, k61, k60, \
+                 k30, k32, k34,      k73, k72, k70                 \
 )                                                       \
 {                                                       \
   {   k00,   k01,   k02,   k03,   k04 },                \
   {   k10,   k11,   k12,   k13,   k14 },                \
   {   k20,   k21,   k22,   k23,   k24 },                \
-  {   k30, KC_NO,   k32, KC_NO, KC_NO },                \
+  {   k30, KC_NO,   k32,   k33, KC_NO },                \
   {   k40,   k41,   k42,   k43,   k44 },                \
   {   k50,   k51,   k52,   k53,   k54 },                \
   {   k60,   k61,   k62,   k63,   k64 },                \
-  {   k70, KC_NO,   k72, KC_NO, KC_NO },                \
+  {   k70, KC_NO,   k72,   k73, KC_NO },                \
 }
 // clang-format on
 
