d80e7a693906e650f22d0589e195bb614bb5ae42
pixls <tha.pixls@gmail.com>
2023-11-25 14:10:06 -0500

    start flanger firmware based on nyquist and skeletyl

---
+++ keyboards/pixls/flanger/readme.md
@@ -0,0 +1,4 @@
+Flanger
+=======
+
+A prototype split 4x12 ortho, but set up "2.5D" with an angled and separated thumb cluster. PCB made with ergogen, files to be uploaded soon.
---
+++ keyboards/pixls/flanger/rev4/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Danny Nguyen (danny@keeb.io)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// #define SPLIT_HAND_PIN GP23
+#define USB_VBUS_PIN GP18
+#define MASTER_LEFT
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
